6ed17101b148923bed5640b921a4c0dd97226d4f
robot269006 <robot269006@yahoo.co.jp>
2017-06-25 21:46:41 +0900

    Run rspec for user association with post
    1. add warden helper in the rails_helper.rb
    2. new test in post_spec, for testing user association -> the post is
    tested by comparing the text that is post as dummy
    3. run rspec -> fails because no association has been made yet
    4. make association on post.rb, user.rb model
    5. but this is again going to fail because there is no user_id column
    in posts table

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -16,7 +16,7 @@ describe 'navigate' do
   describe 'creation' do
     before do
       user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
-      login_as(user, scope: => :user)
+      login_as(user, :scope => :user)
       visit new_post_path
     end
 
@@ -29,6 +29,14 @@ describe 'navigate' do
       fill_in 'post[rationale]', with: "Some rationale"
       click_on "Save"
       expect(page).to have_content("Some rationale")
+    end
+
+    it 'will have a user associated to a post' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "User_association"
+      # "User_association" is written so that test can be run based on that "User_association" text
+      click_on "Save"
+      expect(User.last.posts.last.rationale).to eq("User_association")
     end
   end
 
