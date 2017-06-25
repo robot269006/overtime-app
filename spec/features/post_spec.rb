435ad2c24dcb5b896ec660df496831989138c6ba
robot269006 <robot269006@yahoo.co.jp>
2017-06-25 12:25:41 +0900

    Run rspec for correct routing of post form page
    1. write rspec that visits new_post_path
    2. create “posts#new” action, and create “new.html” view file in posts

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe 'navigate' do
+
   describe 'index' do
     it 'can be reached successfully' do
       visit posts_path
@@ -11,4 +12,12 @@ describe 'navigate' do
       expect(page).to have_content(/Posts/)
     end
   end
+
+  describe 'creation' do
+    it 'has a new form that can be reached' do
+      visit new_post_path
+      expect(page.status_code).to eq(200)
+    end
+  end
+
 end
