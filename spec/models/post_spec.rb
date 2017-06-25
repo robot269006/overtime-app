8bd49d7473165e77cb076f1503bb71724d022349
robot269006 <robot269006@yahoo.co.jp>
2017-06-25 11:27:35 +0900

    Run rspec for Post record validation
    1. edit post_spec.rb -> add test for creation without date and rationale
    2. edit post.rb -> validates_presence_of

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,9 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   describe "creation" do
+    before do
+      @post = Post.create(date: Date.today, rationale: "Anything")
+    end
+
     it "can be created" do
-      post = Post.create(date: Date.today, rationale: "Anything")
-      expect(post).to be_valid
+      expect(@post).to be_valid
+    end
+
+    it "cannot be created without date and rationale" do
+      @post.date = nil
+      @post.rationale = nil
+      expect(@post).to_not be_valid
     end
   end
 end
