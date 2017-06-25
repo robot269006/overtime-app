4d0c4ae58c7cd224830e64376588e190c37ff1c8
robot269006 <robot269006@yahoo.co.jp>
2017-06-25 11:19:38 +0900

    Run rspec for post creation
    1. edit post_spec.rb -> create post, and expect(post).to be_valid

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  describe "creation" do
+    it "can be created" do
+      post = Post.create(date: Date.today, rationale: "Anything")
+      expect(post).to be_valid
+    end
+  end
 end
