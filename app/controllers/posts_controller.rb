a938f1569ae789ea8bf1ad29e201ca03005c45d7
robot269006 <robot269006@yahoo.co.jp>
2017-07-26 21:14:48 +0900

    Enable post deletion
    1. write a test that test for delete button existence
    2. create a space (table heading) on view
    3. create a space (table data) for _post partial as well
    4. set a link that deletes the post
    5. set controller, define destroy method and allow before_action with
    :destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 
-  before_action :set_params, only: [:show, :edit, :update]
+  before_action :set_params, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -33,6 +33,11 @@ class PostsController < ApplicationController
   end
 
   def show
+  end
+
+  def destroy
+    @post.delete
+    redirect_to posts_path, notice: "Your post was deleted"
   end
 
   private
