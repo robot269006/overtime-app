ddc394489b0827107d8b7d33efb7eee16b80ccd0
robot269006 <robot269006@yahoo.co.jp>
2017-07-29 21:08:37 +0900

    Alter permission (fixing bugs)
    1. change build_stubbed -> Post.create
    2. edit controller -> index page to show current_user.posts only
    3. edit delete test -> update post with user_id -> because we limited
    the posts that are posted by the user, it fails the test since it has a
    post_id that is created randomly by factorygirl
    
    *build_stubbed -> doesn’t touch DB, so it just created a user every
    time that part of test is run. so before -> user.id = 1005, after ->
    user.id = 11 -> by not using build_stubbed, it shifted to use @user
    that was created at before do at the beginning

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :set_params, only: [:show, :edit, :update, :destroy]
 
   def index
-    @posts = Post.all
+    @posts = current_user.posts
   end
 
   def new
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -27,11 +27,12 @@ describe 'navigate' do
     end
 
     it 'has a scope so that only post creators can see their posts' do
-      post1 = FactoryGirl.build_stubbed(:post)
-      post2 = FactoryGirl.build_stubbed(:second_post)
-      non_authorized_user = User.create(first_name: 'Non', last_name: 'Authorized', email: "nonauth@test.com",password: "asdfasdf", password_confirmation: "asdfasdf")
-      post_from_other_user = Post.create(date: Date.today, rationale: "asdf", user_id: non_authorized_user.id)
-      byebug
+      post1 = Post.create(date: Date.today, rationale: "asdf", user_id: @user.id)
+      post2 = Post.create(date: Date.today, rationale: "asdf", user_id: @user.id)
+      other_user = User.create(first_name: 'Non', last_name: 'Authorized', email: "nonauth@test.com",password: "asdfasdf", password_confirmation: "asdfasdf")
+      post_from_other_user = Post.create(date: Date.today, rationale: "this shouldn't be seen", user_id: other_user.id)
+      visit posts_path
+      expect(page).to_not have_content(/this shouldn't be seen/)
     end
 
   end
@@ -47,6 +48,7 @@ describe 'navigate' do
   describe 'delete' do
     it 'can be deleted' do
       @post = FactoryGirl.create(:post)
+      @post.update(user_id: @user.id)
       visit posts_path
       click_link("delete_post_#{@post.id}_from_index")
       expect(page.status_code).to eq(200)
