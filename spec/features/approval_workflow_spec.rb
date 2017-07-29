118880f06ba72ab9986ad087a580765b838c74af
robot269006 <robot269006@yahoo.co.jp>
2017-07-29 17:47:21 +0900

    Test for non user to log in
    1. relocate visit edit_post_path to before block
    2. logout as admin user, but login as normal user instead
    3. then test if the non admin user can edit the post
    4. make _status.html partial, and put in the button components
    5. pass in the partials by the use of partials: {@ use -> try login but
    doesn’t allow checkbox }

--- spec/features/approval_workflow_spec.rb
+++ spec/features/approval_workflow_spec.rb
@@ -9,13 +9,22 @@ describe 'navigate' do
   describe 'edit' do
     before do
       @post = FactoryGirl.create(:post)
+      visit edit_post_path(@post)
     end
 
-    it 'has a status that can be edited on the form' do
-      visit edit_post_path(@post)
+    it 'has a status that can be edited on the form by an admin' do
       choose('post_status_approved')
       click_on "Save"
       expect(@post.reload.status).to eq('approved')
+    end
+
+    it 'cannot be edited by a non admin user' do
+      logout(:user)
+      # no need to be instance (@user) since this non admin user is used only in this part of test
+      user = FactoryGirl.create(:user)
+      login_as(user, scope: :user)
+      visit edit_post_path(@post)
+      expect(page).to_not have_content('Approved')
     end
 
   end
