00a57f914825f53d65fe5a4f9fc3c12ad93f6a3d
robot269006 <robot269006@yahoo.co.jp>
2017-07-30 16:40:09 +0900

    Locking records (posts)
    1. logout from @admin_user, login as a non admin user, update the post
    with approved status, visit edit_post_path, and check if the current
    page is the root page -> fails since the post owner still can edit the
    post status
    2. edit post_policy -> allow update method under certain conditions ->
      a. post is approved but it’s admin user
      b. post is not approved and it’s the post owner

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,5 +1,26 @@
 class PostPolicy < ApplicationPolicy
+
+  # post is edited only under 2 conditions:
+  # a. post is approved but it’s admin user
+  # b. post is not approved and it’s the post owner
+
   def update?
-    record.user_id == user.id || admin_types.include?(user.type)
+    return true if post_approved? && admin?
+    return true if user_or_admin && !post_approved?
   end
+
+  private
+
+  def user_or_admin
+    record.user_id == user.id || admin?
+  end
+
+  def admin?
+    admin_types.include?(user.type)
+  end
+
+  def post_approved?
+    record.approved?
+  end
+
 end
