ea2710656f70bef6f9224d870a4c55a7aea40b4a
robot269006 <robot269006@yahoo.co.jp>
2017-07-31 22:50:24 +0900

    Implement regular expression matcher
    1. refactor user_spec test
    2. add character and string length validation tests
    3. add a regexp matcher -> check on regexpal.com -> add /^[0-9]*/ to
    PHONE_REGEX method
    4. this gives error -> js XSS risk -> /\A[0-9]*\Z/
    
    *http://www.regexpal.com/ -> if the “Test String” is highlighted, it
    means that it is a match

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,11 @@ class User < ApplicationRecord
   validates_presence_of :first_name, :last_name, :phone
   has_many :posts
 
+  # PHONE_REGEX is the regular expression matcher, and :phone format is checked with this matcher
+  PHONE_REGEX = /\A[0-9]*\Z/
+  validates_format_of :phone, with: PHONE_REGEX
+  validates :phone, length: { is: 10 }
+
   def full_name
     last_name.upcase + ", " + first_name.upcase
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,7 +9,9 @@ RSpec.describe User, type: :model do
     it "can be created" do
       expect(@user).to be_valid
     end
+  end
 
+  describe "validations" do
     it "cannot be created without first_name" do
       @user.first_name = nil
       expect(@user).to_not be_valid
@@ -25,8 +27,18 @@ RSpec.describe User, type: :model do
       expect(@user).to_not be_valid
     end
 
+    it "requires the phone attribute to only contain integers" do
+      @user.phone = 'mygreatstr'
+      expect(@user).to_not be_valid
+    end
+
+    it "requires the phone attribute to only have 10 characters" do
+      @user.phone = '12345678901'
+      expect(@user).to_not be_valid
+    end
 
   end
+
 
   describe "custom name methods" do
     it 'full name method that combines first and last name' do
