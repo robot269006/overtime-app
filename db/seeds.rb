f7f122e72a86c52767848ea56685a0e1ac9cae5a
robot269006 <robot269006@yahoo.co.jp>
2017-08-01 22:03:18 +0900

    Creating factory for audit_log
    audit_log -> to be sent when the SMS text message is made
    1. set audit_log factory -> test FactoryGirl create! on rails c -e test
    -> but no audit_log table in test environment (as it works in sandbox)
    -> change seeds.rb file
    2. add audit_log to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,3 +22,10 @@ puts "1 Admin User created"
 end
 
 puts "100 posts have been created"
+
+
+100.times do |audit_log|
+  AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
+end
+
+puts "100 audit logs have been created"
