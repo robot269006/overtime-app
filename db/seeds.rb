f9a35e350a61530c734f6f786501c46265194c57
robot269006 <robot269006@yahoo.co.jp>
2017-08-03 23:10:16 +0900

    Pass in live post records to monitoring dashboard
    1. edit styling
    2. add instance to homepage action
    3. show it on view

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,7 @@ puts "1 Admin User created" if AdminUser.present?
 
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
+  Post.create!(date: Date.today, rationale: "#{post} rationale content Lorem ipsum dolor sit amet, at vidit causae latine vis, vim agam dolore periculis ne. Ne brute eirmod ius, dicant virtute eloquentiam eos id. Iudico ancillae et eam, est petentium honestatis ne, brute prompta perfecto ei has. Pro agam vocibus molestie ei. Molestie consequuntur in per, ut viderer nusquam forensibus ius. At consul fastidii sed, alterum suscipit per eu, movet congue definitiones no mea.", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "100 posts have been created"
